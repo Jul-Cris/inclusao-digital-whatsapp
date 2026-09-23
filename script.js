8e0f9ab5a4a1821440480b8e67869446c38d88a4
Jul-Cris <98556404ju@gmail.com>
2026-09-23 19:36:27 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+function mostrarOrientacao() {
+  document.getElementById("resposta").textContent =
+    "Não clique imediatamente. Confirme a identidade da pessoa por outro meio de contato e nunca compartilhe senhas ou códigos.";
+}
